979c14705e1ac349e3d4da8b39059c88f7a1dc64
Damien Altenburg <daltenburg@rrc.ca>
2024-10-24 14:46:00 -0500

    Update to demonstrate function documentation and raising exceptions.

--- module-5/function_standards.py
+++ module-5/function_standards.py
@@ -7,13 +7,43 @@ Example:
 __version__ = "0.11.2024"
 __author__ = "Damien Altenburg"
 
-def format_greeting(first_name: str, last_name: str, salutation="Hello", title="Mr.") -> str:
+def format_greeting(first_name: str, last_name: str, 
+                    salutation="Hello", title="Mr.") -> str:
+    """Returns a formatted greeting based on the specified person's
+    first and last name.
+    
+    Args:
+        first_name (str): The person's first name.
+        last_name (str): The person's last name.
+        salutation (str): The beginning phrase of the greeting. The
+        default is "Hello".
+        title (str): The person's title. The default is "Mr.".
+    
+    Returns:
+        str: A formatted greeting based on the specified person's
+        first and last name.
+
+    Raises:
+        TypeError: Raised when the first name or last name is not
+        a string type.
+        ValueError: Raised when the last name does not contain any
+        non-whitespace characters.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("The first name must be a string.")
+    
+    if not isinstance(last_name, str):
+        raise TypeError("The last name must be a string.")
+    
+    if last_name.strip() == "":
+        raise ValueError("The last name must contain characters.")
+
     first_name = first_name.strip()
 
     if first_name != "":
         first_name += " "
 
-    return f"{salutation}, {title} {first_name}{last_name}"
+    return f"{salutation}, {title} {first_name} {last_name}"
 
 def to_binary(decimal_number: int) -> str:
     binary_number = ""
@@ -29,3 +59,11 @@ def to_binary(decimal_number: int) -> str:
         binary_number = str(binary_digit) + binary_number
 
     return binary_number
+
+def main() -> None:
+
+    greeting = format_greeting("Damien", "Altenburg")
+    print(greeting)
+
+if __name__ == "__main__":
+    main()
