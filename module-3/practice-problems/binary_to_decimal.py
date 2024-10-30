d2308708da4e28deb1a47ea20d9803483667281f
Damien Altenburg <daltenburg@rrc.ca>
2024-10-30 14:12:10 -0500

    Update to refactor source to use a function.

--- module-3/practice-problems/binary_to_decimal.py
+++ module-3/practice-problems/binary_to_decimal.py
@@ -9,16 +9,41 @@ Date: 2024-10-2
 Usage: python binary_to_decimal.py
 """
 
-binary_number: str = input("Enter binary number: ")
+def to_decimal(binary_number: str) -> float:
+    """Returns the decimal version of the binary number.
+    
+    Args:
+        binary_number (str) - The binary number to convert to a decimal.
+        
+    Returns:
+        float - The decimal version of the binary number.
+    """
+    decimal_number: float = 0
+    exponent = len(binary_number)
 
-decimal_number: float = 0
-exponent = len(binary_number)
+    for digit in binary_number:
+        binary_digit: int = int(digit)
 
-for digit in binary_number:
-    binary_digit: int = int(digit)
+        exponent -= 1
 
-    exponent -= 1
+        decimal_number += binary_digit * 2**exponent
 
-    decimal_number += binary_digit * 2**exponent
+    return decimal_number
 
-print(f"The {binary_number} in decimal is {decimal_number}.")
+def main() -> None:
+    """The main function of the program."""
+    # Get the input
+    binary_number: str = input("Enter binary number: ")
+
+    # Do the conversion
+    decimal_number = to_decimal(binary_number)
+
+    # Output the result
+    print(f"The {binary_number} in decimal is {decimal_number}.")
+
+# python script.py
+# from binary_to_decimal import to_decimal
+if __name__ == "__main__":
+    main()
+
+print(__name__)
